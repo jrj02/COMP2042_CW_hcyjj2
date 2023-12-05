5ec3a5d64f85abe7ec0297348851542b6333929c
Jamsyir <jrashdan.johari@gmail.com>
2023-12-05 23:00:09 +0800

    Initial commit

---
+++ src/main/java/brickGame/Block.java
@@ -0,0 +1,156 @@
+package brickGame;
+
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+
+import java.io.Serializable;
+
+public class Block implements Serializable {
+    private static final Block block = new Block(-1, -1, Color.TRANSPARENT, 99);
+
+    public int row;
+    public int column;
+
+
+    public boolean isDestroyed = false;
+
+    private final Color color;
+    public int type;
+
+    public int x;
+    public int y;
+
+    private final int width = 100;
+    private final int height = 30;
+    private final int paddingTop = height * 2;
+    private final int paddingH = 50;
+    public Rectangle rect;
+
+
+    public static int NO_HIT = -1;
+    public static int HIT_RIGHT = 0;
+    public static int HIT_BOTTOM = 1;
+    public static int HIT_LEFT = 2;
+    public static int HIT_TOP = 3;
+
+    public static int BLOCK_NORMAL = 99;
+    public static int BLOCK_CHOCO = 100;
+    public static int BLOCK_STAR = 101;
+    public static int BLOCK_HEART = 102;
+
+
+    public Block(int row, int column, Color color, int type) {
+        this.row = row;
+        this.column = column;
+        this.color = color;
+        this.type = type;
+
+        draw();
+    }
+
+    private void draw() {
+        x = (column * width) + paddingH;
+        y = (row * height) + paddingTop;
+
+        rect = new Rectangle();
+        rect.setWidth(width);
+        rect.setHeight(height);
+        rect.setX(x);
+        rect.setY(y);
+
+        if (type == BLOCK_CHOCO) {
+            Image image = new Image("choco.jpg");
+            ImagePattern pattern = new ImagePattern(image);
+            rect.setFill(pattern);
+        } else if (type == BLOCK_HEART) {
+            Image image = new Image("heart.jpg");
+            ImagePattern pattern = new ImagePattern(image);
+            rect.setFill(pattern);
+        } else if (type == BLOCK_STAR) {
+            Image image = new Image("star.jpg");
+            ImagePattern pattern = new ImagePattern(image);
+            rect.setFill(pattern);
+        } else {
+            rect.setFill(color);
+        }
+
+    }
+
+
+    public int checkHitToBlock(double xBall, double yBall) {
+        if (isDestroyed) {
+            return NO_HIT;
+        }
+
+        double epsilon = 1e-5;
+
+        int ballradius = 10;
+
+        if (xBall + ballradius >= x && xBall - ballradius <= x + width && Math.abs(yBall - (y + height)) < epsilon) {
+            return HIT_BOTTOM;
+        }
+
+        if (xBall + ballradius >= x && xBall - ballradius <= x + width && Math.abs(yBall - y) < epsilon) {
+            return HIT_TOP;
+        }
+
+        if (yBall + ballradius >= y && yBall - ballradius <= y + height && Math.abs(xBall - (x + width)) < epsilon) {
+            return HIT_RIGHT;
+        }
+
+        if (yBall + ballradius >= y && yBall - ballradius <= y + height && Math.abs(xBall - x) < epsilon) {
+            return HIT_LEFT;
+        }
+
+        return NO_HIT;
+    }
+
+
+   /* public int checkHitToBlock(double xBall, double yBall) {
+        if (isDestroyed) {
+            return NO_HIT;
+        }
+
+        // Define a small epsilon value to handle rounding errors
+        double epsilon = 1e-10;
+
+        if (xBall >= x && xBall <= x + width && Math.abs(yBall - (y + height)) < epsilon) {
+            return HIT_BOTTOM;
+        }
+
+        if (xBall >= x && xBall <= x + width && Math.abs(yBall - y) < epsilon) {
+            return HIT_TOP;
+        }
+
+        if (yBall >= y && yBall <= y + height && Math.abs(xBall - (x + width)) < epsilon) {
+            return HIT_RIGHT;
+        }
+
+        if (yBall >= y && yBall <= y + height && Math.abs(xBall - x) < epsilon) {
+            return HIT_LEFT;
+        }
+
+        return NO_HIT;
+    } */
+
+
+    public static int getPaddingTop() {
+        return block.paddingTop;
+    }
+
+    public static int getPaddingH() {
+        return block.paddingH;
+    }
+
+    public static int getHeight() {
+        return block.height;
+    }
+
+    public static int getWidth() {
+        return block.width;
+    }
+
+}
