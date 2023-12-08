f3bca37454295ced77d9a02ab85ec281230fee2b
Jamsyir <131255775+jrj02@users.noreply.github.com>
2023-12-08 22:19:06 +0800

    Create BrickBreakerMenu.java
    Made a new java class for main menu of the game

---
+++ src/main/java/brickGame/BrickBreakerMenu.java
@@ -0,0 +1,42 @@
+package brickGame;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class BrickBreakerMenu extends Application {
+    private Stage stage;
+    private Scene scene;
+    private VBox root;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        root = new VBox();
+        scene = new Scene(root, 500, 500);
+        stage.setScene(scene);
+        stage.show();
+
+        Button start = new Button("Start");
+        start.setOnAction(e -> {
+            try {
+                new Main().start(stage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+
+        Button exit = new Button("Exit");
+        exit.setOnAction(e -> {
+            stage.close();
+        });
+
+        root.getChildren().addAll(start, exit);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
