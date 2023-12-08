2198b16ff2bfb174a81939f87034248a27c70efb
Jamsyir <131255775+jrj02@users.noreply.github.com>
2023-12-08 18:57:55 +0800

    remove the use of threads from show and showMessage method

--- src/main/java/brickGame/Score.java
+++ src/main/java/brickGame/Score.java
@@ -1,75 +1,64 @@
 package brickGame;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
+
 //import sun.plugin2.message.Message;
 
 public class Score {
-    public void show(final double x, final double y, int score, final Main main) {
-        String sign;
-        if (score >= 0) {
-            sign = "+";
-        } else {
-            sign = "";
-        }
+   public void show(final double x, final double y, int score, final Main main) {
+        String sign = (score >= 0) ? "+" : "";
         final Label label = new Label(sign + score);
         label.setTranslateX(x);
         label.setTranslateY(y);
+        main.root.getChildren().add(label);
 
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                main.root.getChildren().add(label);
-            }
-        });
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 21; i++) {
-                    try {
-                        label.setScaleX(i);
-                        label.setScaleY(i);
-                        label.setOpacity((20 - i) / 20.0);
-                        Thread.sleep(15);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        Timeline timeline = new Timeline();
+        for (int i = 0; i < 21; i++) {
+            final int scaleValue = i;
+            KeyFrame keyFrame = new KeyFrame(
+                    Duration.millis(i * 15),
+                    e -> {
+                        label.setScaleX(scaleValue);
+                        label.setScaleY(scaleValue);
+                        label.setOpacity((20 - scaleValue) / 20.0);
                     }
-                }
-            }
-        }).start();
-    }
+            );
+            timeline.getKeyFrames().add(keyFrame);
+        }
 
+        timeline.setOnFinished(e -> main.root.getChildren().remove(label));
+        timeline.play();
+    }
     public void showMessage(String message, final Main main) {
         final Label label = new Label(message);
         label.setTranslateX(220);
         label.setTranslateY(340);
+        main.root.getChildren().add(label);
 
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                main.root.getChildren().add(label);
-            }
-        });
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 21; i++) {
-                    try {
-                        label.setScaleX(Math.abs(i-10));
-                        label.setScaleY(Math.abs(i-10));
-                        label.setOpacity((20 - i) / 20.0);
-                        Thread.sleep(15);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        Timeline timeline = new Timeline();
+        for (int i = 0; i < 21; i++) {
+            final int scaleValue = Math.abs(i - 10);
+            int finalI = i;
+            KeyFrame keyFrame = new KeyFrame(
+                    Duration.millis(i * 15),
+                    e -> {
+                        label.setScaleX(scaleValue);
+                        label.setScaleY(scaleValue);
+                        label.setOpacity((20 - finalI) / 20.0);
                     }
-                }
-            }
-        }).start();
+            );
+            timeline.getKeyFrames().add(keyFrame);
+        }
+
+        timeline.setOnFinished(e -> main.root.getChildren().remove(label));
+        timeline.play();
     }
 
     public void showGameOver(final Main main) {
