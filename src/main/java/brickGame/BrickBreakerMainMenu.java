be893c6ae8285b8a0ec1233218f26684ec4d3f6a
Jamsyir <131255775+jrj02@users.noreply.github.com>
2023-12-09 19:42:52 +0800

    Update BrickBreakerMainMenu.java
    Updated to make it more sophisticated. Added background image and buttons

--- src/main/java/brickGame/BrickBreakerMainMenu.java
+++ src/main/java/brickGame/BrickBreakerMainMenu.java
@@ -1,42 +1,64 @@
 package brickGame;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class BrickBreakerMainMenu extends Application {
-    private Stage stage;
-    private Scene scene;
-    private VBox root;
-
     @Override
     public void start(Stage stage) throws Exception {
-        this.stage = stage;
-        root = new VBox();
-        scene = new Scene(root, 500, 500);
-        stage.setScene(scene);
+        stage.setScene(new Scene(createContent()));
         stage.show();
-
-        Button start = new Button("Start");
-        start.setOnAction(e -> {
-            try {
-                new Main().start(stage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-
-        Button exit = new Button("Exit");
-        exit.setOnAction(e -> {
-            stage.close();
-        });
-
-        root.getChildren().addAll(start, exit);
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    private Parent createContent() {
+        Pane root = new Pane();
+
+        root.setPrefSize(500, 700);
+
+        Image bgImage = new Image(getClass().getResource("/MainMenuImage.jpg").toExternalForm(), 500, 700, false, true);
+
+        VBox box = new VBox(10, new MainMenuButton("Start", () -> {}),
+                new MainMenuButton("LoadSave", () -> {}),
+                new MainMenuButton("Exit", Platform::exit));
+
+        box.setTranslateX(100);
+        box.setTranslateY(200);
+
+        root.getChildren().addAll(new ImageView(bgImage), box);
+
+        return root;
+    }
+
+    private static class MainMenuButton extends StackPane {
+        MainMenuButton(String name, Runnable action) {
+            LinearGradient gradient = new LinearGradient(0, 0.5, 1, 0.5, true, CycleMethod.NO_CYCLE,
+                    new Stop(0.1, Color.web("white", 0.5)),
+                    new Stop(1.0, Color.web("black", 0.5)));
+
+            Rectangle bg = new Rectangle(200, 30, gradient);
+
+            Text text = new Text(name);
+            text.fillProperty().bind(Bindings.when(hoverProperty()).then(Color.WHITE).otherwise(Color.GRAY));
+            getChildren().addAll(bg, text);
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
     }
 }
