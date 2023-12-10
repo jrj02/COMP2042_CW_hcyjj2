8740f1f1b0c093eca338f3b23cf5559c16944a32
Jamsyir <131255775+jrj02@users.noreply.github.com>
2023-12-11 05:15:48 +0800

    Update Sound.java to add in-game music

--- src/main/java/brickGame/Sound.java
+++ src/main/java/brickGame/Sound.java
@@ -8,6 +8,7 @@ import java.io.File;
 
 public class Sound {
     private static final String MUSIC_FILE = "src/main/resources/Sound/MainMenuMusic.mp3";
+    private static final String INGAMEMUSIC_FILE = "src/main/resources/Sound/InGameMusic.mp3";
     private static MediaPlayer mediaPlayer;
 
     public static void playBackgroundMusic() {
@@ -17,7 +18,20 @@ public class Sound {
         mediaPlayer.play();
     }
 
+    public static void playInGameMusic() {
+        Media sound = new Media(new File(INGAMEMUSIC_FILE).toURI().toString());
+        mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+    }
+
     public static void stopBackgroundMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+    }
+
+    public static void stopInGameMusic() {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
         }
