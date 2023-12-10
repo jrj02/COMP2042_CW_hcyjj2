26c418c8812fb2d2ce8fa9864a5b10fe71c09a87
Jamsyir <131255775+jrj02@users.noreply.github.com>
2023-12-11 06:00:57 +0800

    Added 3 new sound effects to Sound.java
    Brick breaking, losing a heart and losing the game

--- src/main/java/brickGame/Sound.java
+++ src/main/java/brickGame/Sound.java
@@ -9,6 +9,9 @@ import java.io.File;
 public class Sound {
     private static final String MUSIC_FILE = "src/main/resources/Sound/MainMenuMusic.mp3";
     private static final String INGAMEMUSIC_FILE = "src/main/resources/Sound/InGameMusic.mp3";
+    private static final String BREAK_FILE = "src/main/resources/Sound/BrickBroken.mp3";
+    private static final String LOSEHEART_FILE = "src/main/resources/Sound/LoseHeart.mp3";
+    private static final String LOSEGAME_FILE = "src/main/resources/Sound/LoseGame.mp3";
     private static MediaPlayer mediaPlayer;
 
     public static void playBackgroundMusic() {
@@ -35,6 +38,27 @@ public class Sound {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
         }
+    }
+
+    public static void playBlockBreakSound() {
+        Media sound = new Media(new File(BREAK_FILE).toURI().toString());
+        MediaPlayer blockBreakMediaPlayer = new MediaPlayer(sound);
+        blockBreakMediaPlayer.play();
+        blockBreakMediaPlayer.setVolume(1.5);
+    }
+
+    public static void playLoseHeartSound() {
+        Media sound = new Media(new File(LOSEHEART_FILE).toURI().toString());
+        MediaPlayer loseHeartMediaPlayer = new MediaPlayer(sound);
+        loseHeartMediaPlayer.play();
+        loseHeartMediaPlayer.setVolume(1.5);
+    }
+
+    public static void playLoseGameSound() {
+        Media sound = new Media(new File(LOSEGAME_FILE).toURI().toString());
+        MediaPlayer loseGameMediaPlayer = new MediaPlayer(sound);
+        loseGameMediaPlayer.play();
+        loseGameMediaPlayer.setVolume(1.5);
     }
 }
 
