2425e9fa64dbb20f71c0403f6823e017e7c0a59a
Jamsyir <131255775+jrj02@users.noreply.github.com>
2023-12-11 14:22:54 +0800

    Added 3 new sounds to Sound.java
    Losing screen song, platform hit sound and power up sound

--- src/main/java/brickGame/Sound.java
+++ src/main/java/brickGame/Sound.java
@@ -12,6 +12,9 @@ public class Sound {
     private static final String BREAK_FILE = "src/main/resources/Sound/BrickBroken.mp3";
     private static final String LOSEHEART_FILE = "src/main/resources/Sound/LoseHeart.mp3";
     private static final String LOSEGAME_FILE = "src/main/resources/Sound/LoseGame.mp3";
+    private static final String LOSESCREEN_FILE = "src/main/resources/Sound/LoseScreenMusic.mp3";
+    private static final String PLATFORMHIT_FILE = "src/main/resources/Sound/PlatformSound.mp3";
+    private static final String POWERUP_FILE = "src/main/resources/Sound/PowerUp.mp3";
     private static MediaPlayer mediaPlayer;
 
     public static void playBackgroundMusic() {
@@ -28,6 +31,15 @@ public class Sound {
         mediaPlayer.play();
     }
 
+    public static void playLoseScreenMusic() {
+        Media sound = new Media(new File(LOSESCREEN_FILE).toURI().toString());
+        mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+        mediaPlayer.setVolume(0.5);
+    }
+
+
     public static void stopBackgroundMusic() {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
@@ -35,6 +47,13 @@ public class Sound {
     }
 
     public static void stopInGameMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            //System.out.println("in game music stop"); to test out a bug. Its been fixed
+        }
+    }
+
+    public static void stopLoseScreenMusic() {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
         }
@@ -59,6 +78,20 @@ public class Sound {
         MediaPlayer loseGameMediaPlayer = new MediaPlayer(sound);
         loseGameMediaPlayer.play();
         loseGameMediaPlayer.setVolume(1.5);
+    }
+
+    public static void playHitPlatformSound() {
+        Media sound = new Media(new File(PLATFORMHIT_FILE).toURI().toString());
+        MediaPlayer blockBreakMediaPlayer = new MediaPlayer(sound);
+        blockBreakMediaPlayer.play();
+        blockBreakMediaPlayer.setVolume(2);
+    }
+
+    public static void playObtainPowerUp() {
+        Media sound = new Media(new File(POWERUP_FILE).toURI().toString());
+        MediaPlayer blockBreakMediaPlayer = new MediaPlayer(sound);
+        blockBreakMediaPlayer.play();
+        blockBreakMediaPlayer.setVolume(1);
     }
 }
 
